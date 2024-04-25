cb6992c757837a9b0cfdd4c5bde4108cce1fdad1
trevor-anderson <trevor@nerdware.cloud>
2024-04-25 12:04:41 -0400

    fix: add quantifier to ensure base64 str is at least 2 chars long

--- src/validate/base64.ts
+++ src/validate/base64.ts
@@ -3,4 +3,4 @@ import { getRegexValidatorFn } from "./_helpers.js";
 /**
  * Returns `true` if `value` is a valid base64-encoded string.
  */
-export const isValidBase64 = getRegexValidatorFn(/^[a-zA-Z0-9+/]+={0,3}$/);
+export const isValidBase64 = getRegexValidatorFn(/^[a-zA-Z0-9+/]{2,}={0,3}$/);
