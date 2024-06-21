fb863909d7ee3687e4a1bdee8c9fca59dfd30641
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-21 09:21:08 -0400

    fix: replace '\s' with '\x20' in fns meant to only allow single-space chars

--- src/sanitize/alphanumericWithSpaces.ts
+++ src/sanitize/alphanumericWithSpaces.ts
@@ -4,5 +4,8 @@ import { getSanitizerFn } from "./_helpers.js";
  * Removes all non-alphanumeric/space characters from `str`.
  *
  * > This function only permits [ASCII characters](https://www.asciitable.com/).
+ *
+ * **Pattern Notes:**
+ * - `\x20` is the ASCII hex code for a single horizontal space.
  */
-export const sanitizeAlphanumericWithSpaces = getSanitizerFn(/[^a-zA-Z0-9\s]/g);
+export const sanitizeAlphanumericWithSpaces = getSanitizerFn(/[^a-zA-Z0-9\x20]/g);
--- src/validate/alphabeticWithSpaces.ts
+++ src/validate/alphabeticWithSpaces.ts
@@ -4,5 +4,8 @@ import { getRegexValidatorFn } from "./_helpers.js";
  * Returns `true` if `value` only contains alphabetic characters and/or spaces.
  *
  * > This function only permits [ASCII characters](https://www.asciitable.com/).
+ *
+ * **Pattern Notes:**
+ * - `\x20` is the ASCII hex code for a single horizontal space.
  */
-export const isValidAlphabeticWithSpaces = getRegexValidatorFn(/^[a-zA-Z\s]+$/);
+export const isValidAlphabeticWithSpaces = getRegexValidatorFn(/^[a-zA-Z\x20]+$/);
