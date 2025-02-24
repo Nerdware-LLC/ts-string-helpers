ed42978de93f7ac00ed12ca3b40c5f98affe021e
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-02-24 04:50:29 -0500

    refactor: impl `test.each`

--- src/validate/alphabetic.test.ts
+++ src/validate/alphabetic.test.ts
@@ -1,21 +1,23 @@
+import { NULL_CONTROL_CHAR } from "../utils/characters.test.js";
 import { isValidAlphabetic } from "./alphabetic.js";
 
 describe("isValidAlphabetic", () => {
   // POSITIVE TEST CASES:
-
   test("returns true when given a string containing only letters", () => {
     expect(isValidAlphabetic("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")).toBe(true);
   });
 
   // NEGATIVE TEST CASES:
-
-  test("returns false when given a string containing a space", () => {
-    expect(isValidAlphabetic("abc ")).toBe(false);
-  });
-  test("returns false when given a string containing numbers", () => {
-    expect(isValidAlphabetic("abc123")).toBe(false);
-  });
-  test("returns false when given a string containing a null unicode character", () => {
-    expect(isValidAlphabetic("abc\u0000")).toBe(false);
+  test.each([
+    { input: "", description: "an empty string" },
+    { input: "abc ", description: "a string containing a space" },
+    { input: "abc1", description: "a string containing a number" },
+    { input: "abc~", description: "a string containing a special character" },
+    { input: `abc${NULL_CONTROL_CHAR}`, description: "a string containing a null unicode char" },
+    { input: 1, description: "a number" },
+    { input: null, description: "null" },
+    { input: undefined, description: "undefined" },
+  ])(`returns false when called with $description`, ({ input }) => {
+    expect(isValidAlphabetic(input)).toBe(false);
   });
 });
--- src/validate/currency.test.ts
+++ src/validate/currency.test.ts
@@ -1,34 +1,43 @@
 import { isValidCurrency } from "./currency.js";
 
 describe("isValidCurrency", () => {
-  // prettier-ignore
-  const baseValidAmountStrings = ["0", "1", "12", "123", "1,234", "12,345", "123,456", "1,234,567", "12,345,678", "123,456,789"];
-
   // POSITIVE TEST CASES:
-
-  test("returns true when given a valid USD currency string", () => {
-    baseValidAmountStrings.forEach((amountStr) => {
-      expect(isValidCurrency(amountStr)).toBe(true);
-      expect(isValidCurrency(`${amountStr}.99`)).toBe(true);
-      expect(isValidCurrency(`$${amountStr}`)).toBe(true);
-      expect(isValidCurrency(`$${amountStr}.99`)).toBe(true);
-    });
+  test.each(
+    [
+      "0",
+      "1",
+      "12",
+      "123",
+      "1,234",
+      "12,345",
+      "123,456",
+      "1,234,567",
+      "12,345,678",
+      "123,456,789",
+    ].reduce<Array<string>>((accum, amountStr) => {
+      accum.push(amountStr, `${amountStr}.99`, `$${amountStr}`, `$${amountStr}.99`);
+      return accum;
+    }, [])
+  )("returns true when called with %s", (input) => {
+    expect(isValidCurrency(input)).toBe(true);
   });
 
   // NEGATIVE TEST CASES:
-
-  test("returns false when given an invalid currency string", () => {
-    expect(isValidCurrency("")).toBe(false); //              empty string
-    expect(isValidCurrency("@$123.45")).toBe(false); //      contains @ symbol
-    expect(isValidCurrency(" $123.45")).toBe(false); //      space before $
-    expect(isValidCurrency("$ 123.45")).toBe(false); //      space between $ and amount
-    expect(isValidCurrency("$123.456")).toBe(false); //      too many decimal places
-    expect(isValidCurrency("\u0000123.45")).toBe(false); //  null unicode character at beginning
-    expect(isValidCurrency("$123.45\u0000")).toBe(false); // null unicode character at end
-    expect(isValidCurrency("12345")).toBe(false); //         missing comma
-    expect(isValidCurrency("123,4")).toBe(false); //         invalid comma placement
-    expect(isValidCurrency("123,45.")).toBe(false); //       invalid comma+decimal placement
-    expect(isValidCurrency("123,45.6")).toBe(false); //      missing decimal digit
-    expect(isValidCurrency("123,45.67")).toBe(false); //     invalid comma placement
+  test.each([
+    { input: "", description: "an empty string" },
+    { input: "$1a", description: "a string that contains letters" },
+    { input: "@$123.45", description: "a string that contains an @ symbol" },
+    { input: " $123.45", description: "a space before the $ symbol" },
+    { input: "$ 123.45", description: "a space after the $ symbol" },
+    { input: "$123.456", description: "too many decimal places" },
+    { input: "\u0000123.45", description: "a null unicode character at the beginning" },
+    { input: "$123.45\u0000", description: "a null unicode character at the end" },
+    { input: "12345", description: "a missing comma" },
+    { input: "123,4", description: "invalid comma placement" },
+    { input: "123.", description: "invalid decimal placement" },
+    { input: "123,45.6", description: "a missing decimal digit" },
+    { input: "", description: "" },
+  ])("returns false when called with $description", ({ input }) => {
+    expect(isValidCurrency(input)).toBe(false);
   });
 });
