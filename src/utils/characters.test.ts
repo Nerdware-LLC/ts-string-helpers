9d0be6c9f7098ab96cd3387724ac9868a2a8f4d7
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-02-24 04:50:54 -0500

    docs: misc jsdoc changes

--- src/utils/characters.test.ts
+++ src/utils/characters.test.ts
@@ -1,9 +1,9 @@
-/** Regex pattern for testing `sanitize` functions which matches all zero-width characters and ASCII/unicode control characters. @internal */
+/** Regex for testing `sanitize` fns that matches all zero-width chars and ASCII/unicode control chars. */
 // eslint-disable-next-line no-control-regex
 export const ZERO_WIDTH_AND_CONTROL_CHAR_REGEX = /[\u200B-\u200D\uFEFF\u0000-\u001F\u007F-\u009F]/g;
 
-/** Zero-width-space character (ASCII code 8203) for testing `sanitize` functions. @internal */
+/** Zero-width-space character (ASCII code 8203) for testing `sanitize` functions. */
 export const ZERO_WIDTH_SPACE = `\u200B`;
 
-/** Null control character (ASCII code 0) for testing `sanitize` functions. @internal */
+/** Null control character (ASCII code 0) for testing `sanitize` functions. */
 export const NULL_CONTROL_CHAR = `\u0000`;
