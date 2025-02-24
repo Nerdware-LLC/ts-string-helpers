b52f04ea65efb0fd27695198b9dcc3c2c108625c
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-02-24 05:15:40 -0500

    test: add tests checking inputs of type 'number'

--- src/validate/numeric.test.ts
+++ src/validate/numeric.test.ts
@@ -1,21 +1,31 @@
+import { NULL_CONTROL_CHAR } from "../utils/characters.test.js";
 import { isValidNumeric } from "./numeric.js";
 
 describe("isValidNumeric", () => {
   // POSITIVE TEST CASES:
-
-  test("returns true when given a string containing only numeric characters", () => {
-    expect(isValidNumeric("0123456789")).toBe(true);
+  test.each([
+    {
+      input: "0123456789",
+      description: "a string containing only numeric characters",
+    },
+    {
+      input: 1,
+      description: "an integer with typeof 'number'",
+    },
+  ])(`returns true when called with $description`, ({ input }) => {
+    expect(isValidNumeric(input)).toBe(true);
   });
 
   // NEGATIVE TEST CASES:
-
-  test("returns false when given a string containing a space", () => {
-    expect(isValidNumeric("123 ")).toBe(false);
-  });
-  test("returns false when given a string containing letters", () => {
-    expect(isValidNumeric("123abc")).toBe(false);
-  });
-  test("returns false when given a string containing a null unicode character", () => {
-    expect(isValidNumeric("123\u0000")).toBe(false);
+  test.each([
+    { input: "", description: "an empty string" },
+    { input: "123 ", description: "a string containing a space" },
+    { input: "123a", description: "a string containing a letter" },
+    { input: "abc~", description: "a string containing a special character" },
+    { input: `abc${NULL_CONTROL_CHAR}`, description: "a string containing a null unicode char" },
+    { input: null, description: "null" },
+    { input: undefined, description: "undefined" },
+  ])(`returns false when called with $description`, ({ input }) => {
+    expect(isValidNumeric(input)).toBe(false);
   });
 });
