e35b7347fce012788883c0fd72e9d75c5858e760
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-02-24 04:52:17 -0500

    fix: add `isString` check to ensure type-safety
    Encountered a problem whereby `isValidAlphabetic` was returning `true` when called with `null`/`undefined`, because the string coercion converted them to "null" and "undefined" respectively. Adding the string type check resolves the issue.

--- src/utils/getValidatorFn.ts
+++ src/utils/getValidatorFn.ts
@@ -1,18 +1,20 @@
+import { isString } from "@nerdware/ts-type-safety-utils";
+
 /**
  * A function which returns `true` if the provided `input` is valid.
  */
 export type ValidatorFn = (value?: unknown) => value is string;
 
 /**
- * Returns a {@link ValidatorFn} which takes an unknown input, and returns `true`
- * if the input is a string that matches the provided regular expression(s).
- *
- * Implementation note: the `RegExp.prototype.test()` method does not throw an
- * error if the input is not a string, so it's unnecessary to check the input
- * type, hence the `as string` type casts.
+ * Returns a {@link ValidatorFn} which takes an unknown input, and returns
+ * `true` if the input is a string that matches the provided regex pattern(s).
  */
 export const getValidatorFn = (regexArg: RegExp | Array<RegExp>): ValidatorFn => {
   return Array.isArray(regexArg)
-    ? (value?: unknown): value is string => regexArg.every((regex) => regex.test(value as string))
-    : (value?: unknown): value is string => regexArg.test(value as string);
+    ? (value?: unknown): value is string => {
+        return isString(value) && regexArg.every((regex) => regex.test(value));
+      }
+    : (value?: unknown): value is string => {
+        return isString(value) && regexArg.test(value);
+      };
 };
